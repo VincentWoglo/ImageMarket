888800a74be09f290b776117299653007d2914ad
VincentWoglo <vincentwoglo@gmail.com>
2020-07-19 23:23:36 -0600

    Image Market

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 
+// setting route
 const port =  process.env.PORT || 1080;
 app.listen(port, ()=>{
-    console.log('hey')
+    console.log('Server is running on port ' + port)
 });
+//joining current path with public folder
 app.use(express.static(path.join(__dirname, "Public")))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/', function (req, res) {
+    
+  })
